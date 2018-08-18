196ad142e3fda371e504246f7670c8a28614c338
Mandar Deo <mandardeo1997@gmail.com>
2018-08-18 20:24:46 +0530

    transposition ciphers Decryption

--- Transposition/doubleColumnar.py
+++ Transposition/doubleColumnar.py
@@ -1,28 +1,75 @@
 def simpleColumnarEncrypt(pt, rows, cols, key = 0):
-        actualKey = ''
-        if key == 0:
-            for i in range(cols):
-                actualKey = actualKey + str(i + 1)
-        else:
-            actualKey = key
-        pt = pt.replace(" ", "").lower()
-        ptList = list(pt)
-        numOfEle = rows * cols
-        for i in range(numOfEle - len(ptList)):
-            ptList.append('x')
-        mat = [ptList[i : i + cols] for i in range(0, numOfEle, cols)]
-        st = ''
-        for i in actualKey:
-            for j in range(rows):
-                st = st + mat[j][int(i) - 1]
-        return st.upper()
-    
+	actualKey = ''
+	if key == 0:
+		for i in range(cols):
+			actualKey = actualKey + str(i + 1)
+	else:
+		actualKey = key.replace(" ", "")
+	pt = pt.replace(" ", "").lower()
+	ptList = list(pt)
+	numOfEle = rows * cols
+	for i in range(numOfEle - len(ptList)):
+		ptList.append('x')
+	mat = [ptList[i : i + cols] for i in range(0, numOfEle, cols)]
+	st = ''
+	for i in actualKey:
+		for j in range(rows):
+			st = st + mat[j][int(i) - 1]
+	return st.upper()
+
+def simpleColumnarDecrypt(ct, rows, cols, key = 0):
+	actualKey = ''
+	if key == 0:
+		for i in range(cols):
+			actualKey = actualKey + str(i + 1)
+	else:
+		actualKey = key.replace(" ", "")
+	ct = ct.replace(" ", "").lower()
+	ctList = [ct[i : i + rows] for i in range(0,rows*cols,rows)]
+	pt = ''
+	dic = {}
+	for index,value in enumerate(actualKey):
+		dic[int(value)] = index
+	for i in range(rows):
+		for j in range(1,cols+1):
+			pt = pt + ctList[dic[j]][i]
+	return pt
+
+
 def doubleColumnarEncrypt(pt, rows, cols, key = 0):
 	st = simpleColumnarEncrypt(simpleColumnarEncrypt(pt, rows, cols, key), rows, cols, key)
 	return st.upper()
-	
-pt = input("Enter Plain Text: ")
-rows = input("Enter number of rows: ")
-cols = input("Enter number of columns: ")
-ct = doubleColumnarEncrypt(pt, int(rows), int(cols))
-print("\nPlain Text : '{0}'\nEncrypted Text: '{1}':".format(pt, ct))
+
+def doubleColumnarDecrypt(ct, rows, cols, key = 0):
+        pt = simpleColumnarDecrypt(simpleColumnarDecrypt(ct, rows, cols, key), rows, cols, key)
+        return pt
+
+while(True):
+	ch = input("\nFor Encryption: Enter 1\nFor Decryption: Enter 2\n")
+	if ch == '1':	
+		pt = input("Enter Plain Text: ")
+		key = 0
+		keyd = input("Do you wish to enter a key? If yes enter 'Y' else enter 'N'\n")
+		if keyd == 'Y' or keyd == 'y':
+			key = input("Enter Key: ")
+		rows = input("Enter number of rows: ")
+		cols = input("Enter number of columns: ")
+		ct = doubleColumnarEncrypt(pt, int(rows), int(cols), key)
+		print("\nPlain Text : '{0}'\nCipher Text: '{1}'".format(pt, ct))
+		
+	elif ch == '2':
+		ct = input("Enter Cipher Text: ")
+		key = 0
+		keyd = input("Do you wish to enter a key? If yes enter 'Y' else enter 'N'\n")
+		if keyd == 'Y' or keyd == 'y':
+			key = input("Enter Key: ")
+		rows = input("Enter number of rows: ")
+		cols = input("Enter number of columns: ")
+		pt = doubleColumnarDecrypt(ct, int(rows), int(cols), key)
+		print("\nCipher Text : '{0}'\nPlain Text: '{1}'".format(ct, pt))
+			
+	else:
+		print("Invalid choice!!")
+	ch = input("\nPress 'Y' to continue, else press 'N'\n")
+	if ch == 'N' or ch == 'n':
+		break
--- Transposition/railfence.py
+++ Transposition/railfence.py
@@ -1,3 +1,5 @@
+import math
+
 def railFenceEncrypt(pt):
         pt = pt.replace(" ", "")
         pt = pt.lower()
@@ -9,7 +11,30 @@ def railFenceEncrypt(pt):
             else:
                 s2 = s2 + ch
         return (s1 + s2).upper()
-		
-pt = input("Enter Plain Text: ")
-ct = railFenceEncrypt(pt)
-print("\nPlain Text : '{0}'\nEncrypted Text: '{1}'".format(pt, ct))
+
+def railFenceDecrypt(ct):
+        ct = ct.replace(" ", "").lower()
+        s1 = ct[0:math.ceil(len(ct)/2)]
+        s2 = ct[math.ceil(len(ct)/2):]
+        pt = ''
+        for c1,c2 in zip(s1,s2):
+            pt = pt + c1 + c2
+        if len(s1) > len(s2):
+            pt = pt + s1[-1]
+        return pt
+
+while(True):
+	ch = input("\nFor Encryption: Enter 1\nFor Decryption: Enter 2\n")
+	if ch == '1':	
+		pt = input("Enter Plain Text: ")
+		ct = railFenceEncrypt(pt)
+		print("\nPlain Text : '{0}'\nCipher Text: '{1}'".format(pt, ct))
+	elif ch == '2':
+		ct = input("Enter Cipher Text: ")
+		pt = railFenceDecrypt(ct)
+		print("\nCipher Text : '{0}'\nPlain Text: '{1}'".format(ct, pt))
+	else:
+		print("Invalid choice!!")
+	ch = input("\nPress 'Y' to continue, else press 'N'\n")
+	if ch == 'N' or ch == 'n':
+		break
